3708aef32b7d6d836d1927c330ba04e2f1fe1ea7
Miniknacky <Jonathanhuybrechts@gmail.com>
2024-03-04 13:44:09 +0100

    task 5

---
+++ models/engine/file_storage.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""Module for File Storage class.
+
+This module provides the FileStorage class which handles serialization and
+deserialization of all class instances to and from a JSON file.
+"""
+
+import json
+from models.base_model import BaseModel
+
+
+class FileStorage:
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """Returns the dictionary __objects."""
+        return FileStorage.__objects
+
+    def new(self, obj):
+        """Sets in __objects the obj with key <obj class name>.id."""
+        key = "{}.{}".format(type(obj).__name__, obj.id)
+        FileStorage.__objects[key] = obj
+
+    def save(self):
+        """Serializes __objects to the JSON file (path: __file_path)."""
+        obj_dict = {obj_id: obj.to_dict()
+                    for obj_id, obj in FileStorage.__objects.items()}
+        with open(FileStorage.__file_path, 'w') as f:
+            json.dump(obj_dict, f)
+
+    def reload(self):
+        """Deserializes the JSON file to __objects."""
+        try:
+            with open(FileStorage.__file_path, 'r') as f:
+                objs = json.load(f)
+            for obj_id, obj in objs.items():
+                cls_name = obj['__class__']
+                del obj['__class__']
+                cls = globals()[cls_name]
+                FileStorage.__objects[obj_id] = cls(**obj)
+        except FileNotFoundError:
+            pass
